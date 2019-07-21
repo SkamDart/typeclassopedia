95077543eb152497a521df9b584f18d5a2581501
Cameron Dart <cdart@anduril.com>
2019-07-21 00:22:17 -0700

    Functor soft questions

--- src/Typeclasses/Functor.hs
+++ src/Typeclasses/Functor.hs
@@ -3,12 +3,17 @@ module Typeclasses.Functor () where
 import qualified Prelude as P
 
 
+const :: a -> b -> a
+const x _ = x
+
+(.) :: (b -> c) -> (a -> b) -> a -> c
+(.) f g = \x -> f (g x)
+
 -- Type class declaration for Functor.
 class Functor f where
   fmap :: (a -> b) -> f a -> f b
   (<$) :: a        -> f b -> f a
-  -- TODO implement const
-  -- (<$) a = fmap P.. P.const
+  (<$) = fmap . const
 
 data Maybe a = Just a
              | Nothing
@@ -35,9 +40,22 @@ instance Functor (Pair) where
 data ITree a = Leaf (P.Int -> a)
              | Node [ITree a]
 
-instance Functor ITree where
-  fmap f (Leaf g) = Leaf g (f g)
-  fmap f (Node x) = fmap f x
+-- instance Functor ITree where
+  -- fmap f (Leaf g) = Leaf g (f g)
+  -- fmap f (Node x) = fmap f x
 
 -- If f :: a -> b is a functor and g :: b -> c is a functor, then
 -- h = (f . g) x = f (g x) is also a functor.
+
+-- Functor Laws
+-- fmap id = id
+-- fmap (g . h) = (fmap g) . (fmap h)
+
+
+-- Evil Functor instance
+-- instance Functor [] where
+  -- fmap :: (a -> b) -> [a] -> [b]
+  -- fmap _ [] = []
+  -- fmap g (x:xs) = g x : g x : fmap g xs
+
+-- This is "Evil" because it does not obey the functor law of identity.
